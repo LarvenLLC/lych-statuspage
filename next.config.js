8d5b1d0d3c6fcaba20f5b991ba13093ceb5afad8
Adam Beleko <bossbele@larven.io>
2024-05-09 11:21:13 +0300

    configured urls - next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-/** @type {import('next').NextConfig} */
 
 const production = process.env.NODE_ENV === "production";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: production ? '/' : '',
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    REPOSITORY_NAME: 'LarvenLLC/lych-statuspage',
+    REPOSITORIES_API_BASE_URL: 'https://api.github.com/repos',
+  },
 }
 
 module.exports = nextConfig
